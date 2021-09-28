68d1d51b0f6786142b50a10e52cf10c9fed31932
2badasea <87015494+2badasea@users.noreply.github.com>
2021-09-28 13:38:32 +0900

    Delete 20210928 directory

--- 20210928/src/main/java/co/micol/prj/MainApp.java
+++
@@ -1,8 +0,0 @@
-package co.micol.prj;
-
-public class MainApp {
-	public static void main(String[] args) {
-		DoWhileTest doWhileTest = new DoWhileTest();
-		doWhileTest.run();         // 이런 방식을 두고 정보은폐라 함. 
-	}
-}
