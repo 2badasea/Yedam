1e4dc584191e650b76796eb5447af7db816fa8b2
badalee <gnjqtpf1@gmail.com>
2021-10-05 22:17:14 +0900

    2ddd

--- 20211004/src/main/java/co/bada/prj/App.java
+++ 20211004/src/main/java/co/bada/prj/App.java
@@ -10,8 +10,11 @@ import co.bada.prj.service.RemoteController;
 import co.bada.prj.tryexam.TryCatchTest;
 
 /**
- * Hello world!
- *
+ * 1. 일반적인 클래스의 경우 필드, 생성자, 메서드를 구성 멤버로 가지는데, 인터페이스 클래스는
+ *	상수 필드와 추상 메소드만을 구성멤버로 가질 수 있다. 또한 인터페이스는 객체를 생성할 수 없기
+ *	때문에 기본적으로 생성자를 가질 수 없다. 
+ * 2. 인터페이스에서 상수 필드값들은 변경이 되지 않는다. 굳이 붙이지 않더라도 컴파일 과정에서
+ * 	알아서 'final static'이 붙는다. 
  */
 public class App 
 {
@@ -21,8 +24,10 @@ public class App
 //   SamsungTV tv  = new SamsungTV(); //자기 자신으로 해버리면 객체교환이 힘들다. 밑의 LgTv가 에러.
     	tv.powerOn();
         tv.powerOff();
+        //현재 tv라는 변수가 samsungtv를 인스턴스화 시킨 것이므로, powerOn/Off를 하면 삼성클래스의 메서드가 실행된다. 
         
      
+        //이번에는 엘지 클래스 설계도를 기반으로 인스턴스를 생성하여 그것을 tv라는 변수에 담았다. 
         tv = new LgTV();
         tv.powerOn();
         tv.powerOff();
@@ -31,7 +36,7 @@ public class App
         Pencil pencil = null;
         pencil = new Pencil4B();
         pencil.selectPencil();
-        pencil.pencilInterface(); //pencilInterface가 가지고 있는 기본 메서드. 
+        pencil.pencilInterface(); //pencilInterface가 가지고 있는 기본 메서드. "나는 인터페이스 객체다" 호출.
         
         pencil = new Pencil6B();
         pencil.selectPencil();
@@ -51,7 +56,7 @@ public class App
         //testinterface부분 수업부터. 
         
         RemoteController rc = new SamsungTV();
-//        rc =ㄴ new LgTV();   //주석을 해제하면 LgTv내용이 출력된다. 부품교제와 유사.
+//        rc = new LgTV();   //주석을 해제하면 LgTv내용이 출력된다. 부품교제와 유사.
         TestInterface ts = new TestInterface(rc);  //'rc'처럼 인스턴스도 매개변수로 가능.
         
         //바로 위를 밑에처럼 표현해도 가능.
--- 20211004/src/main/java/co/bada/prj/product/SamsungTV.java
+++ 20211004/src/main/java/co/bada/prj/product/SamsungTV.java
@@ -1,6 +1,8 @@
 package co.bada.prj.product;
 
 import co.bada.prj.service.RemoteController;
+//import해주는 이유는 인터페이스클래스와 해당  SamsungTV클래스가 소속된 패키지가 다르기 때문이다. 
+//모두 같은 패키지에 속한 클래스라면 별도로 import 안 해도 된다. 
 
 public class SamsungTV implements RemoteController {
 
