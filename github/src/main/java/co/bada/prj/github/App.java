319fa4b6d7c6ff7b08237e64038f106732e01652
badalee <gnjqtpf1@gmail.com>
2021-10-12 22:56:07 +0900

    dfsdffs

---
+++ github/src/main/java/co/bada/prj/github/App.java
@@ -0,0 +1,13 @@
+package co.bada.prj.github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
